77d6ad501d3e143673adec7819a792df4178cc67
Njoki <kinuthiash@gmail.com>
2022-06-16 20:38:43 +0300

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+
+import React, { useState, useEffect } from "react";
+import Search from "./Components/Search"
+
 
 function App() {
+  // const [data , setData] = useState({})
+  const [city,setCity] = useState("Nairobi")
+ 
+  const apiKey = 
+    "28d519b791ec96557c38884a192d1bc7";
+  
+  const data = {
+  "coord": {
+    "lon": -121.9358,
+      "lat": 37.7021
+  },
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+    "base": "stations",
+      "main": {
+    "temp": 289.35,
+      "feels_like": 288.83,
+        "temp_min": 284.83,
+          "temp_max": 293.59,
+            "pressure": 1010,
+              "humidity": 69
+  },
+  "visibility": 10000,
+    "wind": {
+    "speed": 4.63,
+      "deg": 240
+  },
+  "clouds": {
+    "all": 0
+  },
+  "dt": 1655393554,
+    "sys": {
+    "type": 2,
+      "id": 2016191,
+        "country": "US",
+          "sunrise": 1655383539,
+            "sunset": 1655436683
+  },
+  "timezone": -25200,
+    "id": 5344157,
+      "name": "Dublin",
+        "cod": 200
+}
+  // const city = "nairobi"
+  // useEffect(() => {
+  //   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+  //     .then(resp=>resp.json())
+  //     .then(data=>console.log(data))
+  // },[])
+  console.log(data.main)
+
+  function handleCity(cityInput) {
+    setCity(cityInput)
+    console.log(`my city is ${city}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search handleSearch={handleCity} />
+      weather        
     </div>
   );
 }
---
+++ src/Components/Search.js
@@ -0,0 +1,29 @@
+import React , {useState} from "react";
+
+function Search({ handleSearch}) {
+  const [search, setSearch] = useState("")
+  
+  function handleChange(event){
+    setSearch(event.target.value)
+    console.log(search)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()    
+    console.log(event.target.value)    
+    handleSearch(search)
+  }
+
+  return (
+    <form className="formInput" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Input a city..."
+        value={search}
+        onChange={handleChange}
+      required/>
+    </form>
+  )
+}
+
+export default Search;
